2001cb0e7926d9e325f67819bdcb1614c0e8fce1
DavidCT <DavidCuervoTunon@gmail.com>
2023-10-12 17:39:30 +0200

    Añadiendo los primeros archivos

---
+++ Aplication/MntoClientes/MntoClientes/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Try
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+End Class
