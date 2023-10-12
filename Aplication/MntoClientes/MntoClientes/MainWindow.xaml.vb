042d9d3304188b1fc57a1d287234f2aeaa6dfd95
DavidCT <DavidCuervoTunon@gmail.com>
2023-10-12 17:48:30 +0200

    Creando las llamadas al servicio web

--- Aplication/MntoClientes/MntoClientes/MainWindow.xaml.vb
+++ Aplication/MntoClientes/MntoClientes/MainWindow.xaml.vb
@@ -1,4 +1,14 @@
-﻿Class MainWindow
+﻿Imports System.Net.Http
+Imports System.Text
+
+Class MainWindow
+
+    Private ReadOnly _baseUri As String
+
+    Public Sub New(baseUri As String)
+        _baseUri = baseUri
+    End Sub
+
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         Try
 
@@ -6,4 +16,19 @@
             MessageBox.Show(ex.ToString)
         End Try
     End Sub
+
+    Public Async Function CallWebServiceAsync(jsonData As String) As Task(Of String)
+        Using client As New HttpClient()
+            client.BaseAddress = New Uri(_baseUri)
+            Dim content As New StringContent(jsonData, Encoding.UTF8, "application/json")
+            Dim response = Await client.PostAsync("ruta_del_servicio", content)
+            If response.IsSuccessStatusCode Then
+                Dim responseContent = Await response.Content.ReadAsStringAsync()
+                Return responseContent
+            Else
+                Console.WriteLine("Error en la solicitud: " & response.StatusCode.ToString())
+                Return Nothing
+            End If
+        End Using
+    End Function
 End Class
